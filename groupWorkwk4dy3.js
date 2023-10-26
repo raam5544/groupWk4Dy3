46bf1296eee63b7141f7e3b5fbd9a6ca14480acf
raam <jeya.colombo@gmail.com>
2023-10-26 14:16:24 -0400

    Find the Median answered

--- groupWorkwk4dy3.js
+++ groupWorkwk4dy3.js
@@ -162,4 +162,14 @@ const msg = ["...human...why you taking pictures of me?...", "...the catnip made
 //     }
 // }
 
+// Find the Median  
+// Find the median number in the following numsarray, then console.log that number.
+// hint if you check the length of the array / 2, you might get not get a whole number. In which case, look into Math.floor( // something )
+const nums = [14, 11, 16, 15, 13, 16, 15, 17, 19, 11, 12, 14, 19, 11, 15, 17, 11, 18, 12, 17, 12, 71, 18, 15, 12];
 
+nums.sort()
+// console.log(nums)
+// console.log(nums[Math.floor(nums.length / 2)])
+
+// Expected output:
+// => 15
