59d7f055177b52db45a6d7b94c8416e71a4e8c98
raam <jeya.colombo@gmail.com>
2023-10-26 10:27:53 -0400

    Get Even answered

--- groupWorkwk4dy3.js
+++ groupWorkwk4dy3.js
@@ -8,3 +8,12 @@
 for (let i = 1; i <= 20; i++) {
     // console.log(i)
 }
+
+// Get Even
+// Write a for loop that will log only the even numbers in 0 through 200.
+
+// Hint: Think about the increment expression.
+
+for (let i = 0; i <= 200; i += 2) {
+    // console.log(i)
+}
